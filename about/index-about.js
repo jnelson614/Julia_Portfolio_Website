c6ba3c8b66fd6c17ce0ca456a876f9a7a93ebea1
Julia Paranay <jeparanay@comcast.net>
2018-03-12 19:17:57 -0400

    about page emojiis!

--- about/index-about.js
+++ about/index-about.js
@@ -1,138 +1,48 @@
 $(document).ready(function(){
 
-    var $container = $('.container');
-    var $console = $('.console');
-    var $ball = $('.ball');
+var emojiArray = [
+  'images/about/emoji/game.png',
+  'images/about/emoji/superman.png',
+  'images/about/emoji/art.png',
+  'images/about/emoji/bike.png',
+  'images/about/emoji/avocado.png',
+  'images/about/emoji/sun.png',
+  'images/about/emoji/cat.png',
+  'images/about/emoji/dog.png',
+  'images/about/emoji/ski.png',
+  'images/about/emoji/think.png',
+  'images/about/emoji/music.png',
+  'images/about/emoji/travel.png',
+  'images/about/emoji/photo.png',
+  'images/about/emoji/video.png',
+  'images/about/emoji/black.png',
+  'images/about/emoji/book.png',
+  'images/about/emoji/fav-food.png'
+];
 
-    $container.left = 0;
-    $container.top = 0;
-    $container.right = $container.width();
-    $container.bottom = $container.height();
+$(document).ready(function(){
 
-    $ball.width = $ball.width();
-    $ball.height = $ball.height();
+  function startEmojiDrop(){
+  	$(".emoji").remove();
+  	dropAnEmoji("slow");
+  }
+  startEmojiDrop();
 
-    $ball.vel = {};
-    $ball.vel.x = 10;
-    $ball.vel.y = -5;
-
-    $ball.pos = {};
-    $ball.pos.x = $container.right/2;
-    $ball.pos.y = $container.bottom/2;
-
-    $ball.bounce = -0.68;
-    $ball.gravity = 0.19;
-
-    $ball.friction = {}
-    $ball.friction.x = 0.004;
-    $ball.friction.y = 0.005;
-
-    $.prototype.calcPhysics = function () {
-    $ball.vel.y += $ball.gravity;
-
-    var isBounce = false;
-    if ($ball.pos.x + $ball.width/2 > $container.right) {
-    $ball.pos.x = $container.right - $ball.width/2;
-    $ball.vel.x *= $ball.bounce;
-    isBounce = true;
-
-    } else if ($ball.pos.x - $ball.width/2 < $container.left) {
-    $ball.pos.x = $container.left + $ball.width/2;
-    $ball.vel.x *= $ball.bounce;
-    isBounce = true;
+  function dropAnEmoji(speed){
+    var randomEmojis = emojiArray[Math.floor(Math.random() * emojiArray.length)];
+  	if($(".emoji").length<=30){
+      $("#emoji-drop-game").prepend('<img class="emoji '+speed+'-falling-emoji" style="margin-left:'+randomStartPosition()+'px" src="'+randomEmojis+'" alt="Emoji">');
     }
+  }
 
-    if ($ball.pos.y + $ball.height/2 > $container.bottom) {
-    $ball.pos.y = $container.bottom - $ball.height/2;
-    $ball.vel.y *= $ball.bounce;
-    isBounce = true;
+  function randomStartPosition(){
+  	return Math.random()*($("#emoji-drop-game").width()-20);
+  }
 
-    } else if ($ball.pos.y - $ball.height/2 < $container.top) {
-    $ball.pos.y = $container.top + $ball.height/2;
-    $ball.vel.y *= $ball.bounce;
-    isBounce = true;
-    }
-
-    if ($ball.vel.x >= 0) {
-    $ball.vel.x -= $ball.friction.x;
-    } else if ($ball.vel.x <= 0) {
-    $ball.vel.x += $ball.friction.x;
-    }
-
-    if ($ball.vel.y >= 0) {
-    $ball.vel.y -= $ball.friction.y;
-    } else if ($ball.vel.y <= 0) {
-    $ball.vel.y += $ball.friction.y;
-    }
-
-    if (isBounce) {
-    $ball.velocity({
-    backgroundColor: '#2ecc71'
-    }, {
-    duration: 100,
-    queue: false,
-    complete: function (e) {
-    $ball.velocity({
-      backgroundColor: '#2980b9'
-    }, {
-      duration: 500,
-      queue: false
-    });
-    }
-    });
-    isBounce = false;
-    }
-
-    $ball.pos.x += $ball.vel.x;
-    $ball.pos.y += $ball.vel.y;
-    };
-
-    $.prototype.setPos = function (pos) {
-    if (!pos.x) {
-    pos.x = pos[0];
-    }
-    if (!pos.y) {
-    pos.y = pos[1];
-    }
-    $(this).css('top', pos.y);
-    $(this).css('left', pos.x);
-    };
-
-    var update = function () {
-    $ball.calcPhysics();
-    $ball.setPos($ball.pos);
-
-    $console.children('.velocity').html('Velocity: <br> x:' + $ball.vel.x + '</br> y:' + $ball.vel.y);
-
-    $console.children('.position').html('Position: <br> x:' + parseInt($ball.pos.x) + '</br> y:' + parseInt($ball.pos.y));
-
-    requestAnimationFrame(update);
-    };
-    update();
-
-    $('.btn.bounce').click(function () {
-    if ($ball.vel.x > 0) {
-    $ball.vel.x += 1;
-    } else if ($ball.vel.x < 0) {
-    $ball.vel.x -= 1;
-    } else {
-    $ball.vel.x = 3;
-    }
-
-    if ($ball.vel.y > 0) {
-    $ball.vel.y += 4;
-    } else if ($ball.vel.y < 0) {
-    $ball.vel.y -= 4;
-    }
-    });
-
-    $('.btn.stop').click(function () {
-    $ball.vel.x = 0;
-    $ball.vel.y = 0;
-    });
-
-
-
+  	$("#emoji-drop-game").on("animationEnd webkitAnimationEnd",".emoji",function(){
+  		dropAnEmoji("medium");
+  	});
+  });
 
 
 
